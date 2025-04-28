d564d2e3018cea5da35a4a5aca5941c8df801ad4
cuzimapanda2003 <marcblais03@gmail.com>
2025-04-28 16:31:02 -0400

    Update Alarm.cpp

--- labo07/Alarm.cpp
+++ labo07/Alarm.cpp
@@ -1,10 +1,73 @@
 #include "Alarm.h"
 
 Alarm::Alarm(int rPin, int gPin, int bPin, int buzzerPin, float* distancePtr)
-  : _rPin(rpin), _gPin(gPin), _bPin(bPin), _buzzerPin(buzzerPin), _distance(distancePtr) {
+  : _rPin(rPin), _gPin(gPin), _bPin(bPin), _buzzerPin(buzzerPin), _distance(distancePtr) {
   pinMode(_rPin, OUTPUT);
   pinMode(_gPin, OUTPUT);
   pinMode(_bPin, OUTPUT);
   pinMode(_buzzerPin, OUTPUT);
-  _turnOff();
+}
+
+void Alarm::update() {
+  switch (_state) {
+    case OFF:
+      _offState();
+      break;
+    case WATCHING:
+      _watchState();
+      break;
+    case ON:
+      _onState();
+      break;
+    case TESTING:
+      _testingState();
+      break;
+  }
+}
+
+void Alarm::setColourA(int r, int g, int b) {
+  
+}
+
+void Alarm::setColourB(int r, int g, int b) {
+}
+
+void Alarm::setVariationTiming(unsigned long ms) {
+}
+
+void Alarm::setDistance(float d) {
+}
+
+void Alarm::setTimeout(unsigned long ms) {
+}
+
+void Alarm::turnOff() {
+}
+
+void Alarm::turnOn() {
+}
+
+void Alarm::test() {
+}
+
+AlarmState Alarm::getState() const {
+  return _state;
+}
+
+void Alarm::_setRGB(int r, int g, int b) {
+}
+
+void Alarm::_turnOff() {
+}
+
+void Alarm::_offState() {
+}
+
+void Alarm::_watchState() {
+}
+
+void Alarm::_onState() {
+}
+
+void Alarm::_testingState() {
 }
