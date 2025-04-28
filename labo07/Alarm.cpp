320c55f4948123b8f24e0cad50ba9d4eb7a94aaf
cuzimapanda2003 <marcblais03@gmail.com>
2025-04-28 15:37:26 -0400

    Update Alarm.cpp

--- labo07/Alarm.cpp
+++ labo07/Alarm.cpp
@@ -1,3 +1,10 @@
 #include "Alarm.h"
 
-Alarm::Alarm(int rPin, int gPin, int bPin, int buzzerPin, float* distancePtr);
+Alarm::Alarm(int rPin, int gPin, int bPin, int buzzerPin, float* distancePtr)
+  : _rPin(rpin), _gPin(gPin), _bPin(bPin), _buzzerPin(buzzerPin), _distance(distancePtr) {
+  pinMode(_rPin, OUTPUT);
+  pinMode(_gPin, OUTPUT);
+  pinMode(_bPin, OUTPUT);
+  pinMode(_buzzerPin, OUTPUT);
+  _turnOff();
+}
