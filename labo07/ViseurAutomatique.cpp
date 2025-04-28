a1cdc3bf7153f40751e7c7677a3343a051ee0678
cuzimapanda2003 <marcblais03@gmail.com>
2025-04-28 16:55:17 -0400

    Create ViseurAutomatique.cpp

---
+++ labo07/ViseurAutomatique.cpp
@@ -0,0 +1,8 @@
+#include "ViseurAutomatique.h"
+
+ViseurAutomatique::ViseurAutomatique(int p1, int p2, int p3, int p4, float& distanceRef)
+  : _stepper(AccelStepper::HALF4WIRE, p1, p3, p2, p4),
+    _distance(distanceRef) {
+  _stepper.setMaxSpeed(500.0);
+  _stepper.setAcceleration(100.0);
+}
